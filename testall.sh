42d8a003baef480964596c29ee14a6e7d365700b
Colin Patrick Mccabe <cmccabe@alumni.cmu.edu>
2012-08-30 18:42:24 -0700

    update to deal with "honor -DskipTests" etc.
    Signed-off-by: Colin McCabe <cmccabe@alumni.cmu.edu>

--- testall.sh
+++ testall.sh
@@ -6,8 +6,18 @@ die() {
 }
 
 rm -rf target/*
-mvn compile test -X -e
+mvn install -X -e -DskipTests
+
+[ -f target/results/myFailingTest.status ] \
+    && die "myFailingTest.status should not exist yet"
+mvn test -Dtest=myFailingTest -X -e  # run just one test
 grep -q 'TIMED_OUT' target/results/myFailingTest.status
+[ -f target/results/passingTest.status ] \
+    && die "passingTest.status should not exist yet"
+mvn test -X -e # should run all tests 
+grep -q 'ERROR 2' target/results/myReturnTest.status
+rm -rf target/*
+mvn compile test -X -e  # run al tests, again
 grep -q 'SUCCESS' target/results/passingTest.status
 grep -q 'ERROR 2' target/results/myReturnTest.status
 grep -q 'passingTest arg1 arg2' target/results/passingTest.stderr
